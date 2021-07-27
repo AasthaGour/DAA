56bebe4a4f43baeaf6bc5fcd04fabac0760ddc3b
Rahul Chamoli <60114471+WinRAUL@users.noreply.github.com>
2021-07-27 13:49:16 +0530

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,36 +1,15 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-	string s;
-	int c3=0;
-	int c5=0;
-	for(int i=1;i<=100;i++)
-		{
-			c3++;
-			c5++;
-			s="";
-			if(c3==3) 
-				{
-					s += "fizz";
-					c3=0;
-				}
-
-			if(c5==5)
-			{
-				s += "buzz";
-				c5=0;
-			}
-				if(s =="")
-					{
-						cout<<i<<endl;
-					}
-					else
-						{
-							cout<<s<<endl;
-						}
-		}
-		return 0;
-
+int main(){
+    int i;
+    for(i=1;i<=100;i++){
+        if(i%3==0)
+        	cout<<"Fizz";
+        if(i%5==0)
+        	cout<<"Buzz";
+        if(i%3!=0 && i%5!=0)
+        	cout<<i;
+        cout<<"\n";
+    }
+    return 0;
 }
-
