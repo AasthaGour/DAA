4c34806d8c85e90ba99b433f12c92835e9c9713a
Aastha Gour <aasthagour@Aasthas-MacBook-Air.local>
2021-08-20 00:42:36 +0530

    Find common elements

---
+++ Hackerearth_test_19_August_2021/p3.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+void find_common(int a1[],int a2[],int n, int m)
+{
+	int i=0,j=0;
+	while(i<n && j<m)
+	{
+	   
+	    while(i<n && (a1[i]==a1[i+1])){
+	        i++;
+	    }
+	    while(j<m && (a2[j]==a2[j+1])){
+	        j++;
+	    }
+		if(a1[i]<a2[j])
+		{
+			i++;
+		}
+		else if (a2[j]<a1[i])
+		{
+			j++;
+		}
+		else 
+		{
+			cout<<a2[j]<<"  ";
+			i++;
+			j++;
+
+		}
+	
+		
+	}
+
+}
+int main()
+{
+	int m,n;
+	cin>>n;
+	
+	int a1[n];
+	
+	for(int i=0;i<n;i++)
+		{
+			cin>>a1[i];
+		}
+        cin>>m;
+        int a2[m];
+	for(int j=0;j<m;j++)
+		{
+			cin>>a2[j];
+		}
+    
+		find_common(a1,a2,n, m);
+		
+		return 0;
+}
